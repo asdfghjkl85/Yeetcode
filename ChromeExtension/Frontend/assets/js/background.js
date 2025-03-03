7fb7ccfd51b2967cbf8d5723e9226c3498ff4c56
bbmaya5 <aidenamaya5@gmail.com>
2025-03-03 18:17:18 -0500

    Side panel functionality
    Made it so the side panel opens when user clicks the google extension icon.

--- ChromeExtension/Frontend/assets/js/background.js
+++ ChromeExtension/Frontend/assets/js/background.js
@@ -1,3 +1,8 @@
+chrome.runtime.onInstalled.addListener(() =>  {
+    chrome.sidePanel.setOptions({ path: "Frontend/main-screen.html" })
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true});
+});
+
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     console.log("\n\nTesting: BACKGROUND LOGS!!!\n\n")
     chrome.tabs.sendMessage(tabId, {
