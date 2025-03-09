3e84c36bd34d0d3b504b52ccbecb178578ebdfd6
Tofudog <leodefarias25@gmail.com>
2025-03-09 19:02:58 -0400

    Reverting back to original validateUser.js

--- ChromeExtension/Backend/utils/validateUser.js
+++ ChromeExtension/Backend/utils/validateUser.js
@@ -9,16 +9,10 @@ const isValid = async (username) => {
         const url = `https://leetcode-api-faisalshohag.vercel.app/${username}`;
         const response = await fetch(url);
         const data = await response.json();
-
+        
         // Check if the response contains errors
-        // It could be a network error though.
-        if (data.hasOwnProperty("errors")) {
-            const errors = data["errors"];
-            const firstError = errors[0];
-            if (firstError["message"].includes("user does not exist")) {
-                return false;
-            }
-            return false; //Keep this for now
+        if (data.errors) {
+            return false;
         }
 
         return true;
